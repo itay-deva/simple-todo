fcb0bff332079d963b1dddaa30c1bb6a2946f763
Itay Koren <CL_itay.koren@rnd-hub.com>
2018-08-03 13:34:42 +0300

    fixed bug in selector

--- src/containers/Todo/index.js
+++ src/containers/Todo/index.js
@@ -3,8 +3,9 @@ import { createSelector } from 'reselect'
 import { addTodoAction, removeTodoAction } from './todos.actions'
 import TodoList from './views/TodoList'
 
-const totodsSelector = createSelector((state) => state.todos )
-const mapStateToProps = state => ({todos: totodsSelector(state)})
+const getTodos = (state) => state.todos;
+const totodsSelector = createSelector(getTodos,(todos) =>  todos);
+const mapStateToProps = state => ({todos: totodsSelector(state)});
 
 const mapDispatchToProps = dispatch => ({
   addTodoAction: (todoText) => dispatch(addTodoAction(todoText)),
