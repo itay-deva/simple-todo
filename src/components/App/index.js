486511ce83be3fa0ed2c9b3f086fb3ddc57c6074
Itay Koren <CL_itay.koren@rnd-hub.com>
2018-08-05 10:19:01 +0300

    removed name

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,7 +5,7 @@ import './App.css';
 const App = () => (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Payoneer Todo List</h1>
+        <h1 className="App-title">Todo List</h1>
       </header>
       <br/>
       <TodoList/>
