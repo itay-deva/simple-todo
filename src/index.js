bdfb580194c5ec1d4b57c6cd970a6e8df9193206
Itay Koren <CL_itay.koren@rnd-hub.com>
2018-08-05 10:11:26 +0300

    moved-index.css to assets/styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'
 import rootReducer from './reducers'
-import './index.css';
+import './assets/styles/index.css';
 import App from './components/App';
 
 const store = createStore(rootReducer)
