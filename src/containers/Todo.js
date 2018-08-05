600e971a240b4f4fb8d719241e2c9f17fda5b6d2
Itay Koren <CL_itay.koren@rnd-hub.com>
2018-08-05 15:29:52 +0300

    adding component to the container

--- src/containers/Todo.js
+++ src/containers/Todo.js
@@ -1,6 +1,8 @@
+import React from 'react'
 import { connect } from 'react-redux';
 import { addTodoAction, removeTodoAction } from '../actions/todos.actions';
-import TodoList from '../components/TodoList';
+import AddTodo from '../components/AddTodo';
+import TodoItem from '../components/TodoItem';
 import totodsSelector from '../selectors/totos.selectors';
 
 const mapStateToProps = state => ({todos: totodsSelector(state)});
@@ -9,6 +11,22 @@ const mapDispatchToProps = dispatch => ({
   addTodoAction: (todoText) => dispatch(addTodoAction(todoText)),
   removeTodoAction: (todoId) => dispatch(removeTodoAction(todoId))
 })
+
+
+const TodoList = (props) => {
+
+  // the reasonfor input is to change it with out making setstate, which will cause a render
+    return (
+      <div>
+        <AddTodo addTodoAction={props.addTodoAction}/>
+        <ul>
+          {props.todos.map((todo) =>
+            <TodoItem todo={{...todo}} clickRemoveAction={props.removeTodoAction}/>
+          )}
+        </ul>
+      </div>
+    )
+}
 
 export default connect(
   mapStateToProps,
